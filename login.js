40a0ae73f68f625b0ee2f1eb605229bfa0023cf7
al20070 <106135839+al20070@users.noreply.github.com>
2022-06-04 16:43:52 +0900

    認証処理

---
+++ login.js
@@ -0,0 +1,12 @@
+import {loginCheck} from "Login";
+import {createAccountCheck} from "CreateAccount";
+
+function login(userID,password){
+    let _loginCheck=loginCheck();
+    return _check;
+}
+
+function createAccount(userID,password){
+    let _createAccountCheck=createAccountCheck();
+    return _check;
+}
