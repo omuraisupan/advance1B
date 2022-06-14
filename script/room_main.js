a42f4c51fcb5c895597d8b060da0649b631f4f3c
N <al20032@shibaura-it.ac.jp>
2022-06-14 14:30:21 +0900

    なかじま

---
+++ script/room_main.js
@@ -0,0 +1,15 @@
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-auth.js"
+import { getDatabase, ref, push, child, remove, onValue } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js";
+import { app } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-config.js"
+
+
+function joinRoom(){
+    ID = (ゆーざIDを取り出す);
+if (ルーム内の人数<=6){
+location.href = "URLw6メインメニュー";
+return ID;  
+}   
+}
+
+
+//まざきへ　げーむさんかぼたんを押したらこの関数を呼び出してください
