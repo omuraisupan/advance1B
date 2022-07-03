14db71e016b0de220b000c4fb1ecbbaa8b0263a9
al20070 <106135839+al20070@users.noreply.github.com>
2022-07-03 16:39:04 +0900

    ランキングとその表示追加しました

--- public/account/highScore.js
+++ public/account/highScore.js
@@ -1,5 +1,6 @@
-import { ref, set, onValue, getDatabase } from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js"
+import { ref, set, onValue, getDatabase} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js"
 import { app } from "./app.js"
+let score = 0;
 
 /* 6.18 kakishima
 
@@ -8,20 +9,22 @@ arg : none
 ret : none
 ------------------------
 プライベート変数
-_userID: ページに入力されたユーザIDを格納
-_password: ページに入力されたパスワードを格納
-_database: 呼び出したデータベースを格納
+_userID: ページに入力されたユーザID
+_password: ページに入力されたパスワード
+_database: 呼び出したデータベース
 _ref: databaseの場所
 _url: 現在のページのURL
+_highScore: 入力されたハイスコア
+_highScore0: 現時点でのハイスコア
 ------------------------
 そのユーザIDの持ち主のハイスコアを更新する
 成功：updata highScore successと表示し、highScoreを更新する
 失敗：updata highScore failedと表示する
 */
 
-highScore.addEventListener('click',(e) => {
-  const _highScore = document.getElementById('highScore').value;
-  //const _database = returnDatabase();
+//inputHighScore.addEventListener('click',(e) => {
+  //const _highScore = document.getElementById('highScore').value;
+export function inputHighScore(_highScore){
   const _database = getDatabase(app);
   const _url = window.location.href; //URLの取得
   const _userID = getParam("userID", _url);
@@ -30,18 +33,25 @@ highScore.addEventListener('click',(e) => {
   onValue(_ref, (snapshot) => {
     const _data = snapshot.val();
     const _password = _data.password;
-    set(_ref, {
-      password: _password,
-      highScore: _highScore
-    })
-    .then(() => {
-      console.log('updata highScore success');
-    })
-    .catch((error) => {
-      console.log('updata highScore failed');
-    });
+    const _highScore0 = parseInt(_data.highScore);
+    setHighScore(_highScore);
+    if(_highScore0 < _highScore){
+      set(_ref, {
+        password: _password,
+        highScore: _highScore
+      })
+      .then(() => {
+        console.log('updata highScore success');
+      })
+      .catch((error) => {
+        console.log('updata highScore failed');
+      });
+    }else{
+      console.log("inputhighscore 更新なし");
+    }
   });
-})
+}
+//)
 
 
 /* 6.26 kakishima
@@ -58,11 +68,90 @@ _results: urlから_regexの内容があるかを検索した結果
 URLからnameで指定した項目を出力する
 */
 
-function getParam(name, url) {
+export function getParam(name, url) {
   name = name.replace(/[\[\]]/g, "\\$&");
   var _regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
   var _results = _regex.exec(url);
   if (!_results) return null;
   if (!_results[2]) return '';
   return decodeURIComponent(_results[2].replace(/\+/g, " "));
+}
+
+
+/* 6.30 kakishima
+
+------------------------
+arg : _highScore = 入力されたハイスコア(ゲーム終了時のスコア)
+ret : none
+------------------------
+プライベート変数
+_database: 呼び出したデータベース
+_ref: databaseの場所
+_url: 現在のページのURL
+_highScore0: 入力されたハイスコア(ゲーム終了時のスコア)
+_highScore1: 現時点で1位のハイスコア
+_highScore2: 現時点で2位のハイスコア
+_highScore3: 現時点で3位のハイスコア
+_userID0: urlから取得した現在のプレイ中のユーザID
+_userID1: 現時点で1位のユーザID
+_userID2: 現時点で2位のユーザID
+_userID3: 現時点で3位のユーザID
+------------------------
+全体のハイスコアを更新
+*/
+
+function setHighScore(_highScore){
+  let _highScore0 = _highScore;
+  const _database = getDatabase(app);
+  const _ref = ref(_database, '/data/' + 'ranking/');
+  onValue(_ref, (snapshot) => {
+    const _url = window.location.href;
+    let _userID0 = getParam("userID", _url);
+    let _highScore1 = parseInt(snapshot.val().highScore1);
+    let _userID1 = snapshot.val().userID1;
+    let _highScore2 = parseInt(snapshot.val().highScore2);
+    let _userID2 = snapshot.val().userID2;
+    let _highScore3 = parseInt(snapshot.val().highScore3);
+    let _userID3 = snapshot.val().userID3;
+    
+    if(_highScore3 < _highScore0){
+    let _change = _highScore0;
+    _highScore0 = _highScore3;
+    _highScore3 = _change;
+    _change =_userID0;
+    _userID0 = _userID3;
+    _userID3 = _change;
+      if(_highScore2 < _highScore3){
+        _change = _highScore3;
+        _highScore3 = _highScore2;
+        _highScore2 = _change;
+        _change =_userID3;
+        _userID3 = _userID2;
+        _userID2 = _change;
+        if(_highScore1 < _highScore2){
+          _change = _highScore2;
+          _highScore2 = _highScore1;
+          _highScore1 = _change;
+          _change =_userID2;
+          _userID2 = _userID1;
+          _userID1 = _change;
+        }
+      }
+    }
+    set(_ref, {
+      highScore1: _highScore1,
+      highScore2: _highScore2,
+      highScore3: _highScore3,
+      userID1: _userID1,
+      userID2: _userID2,
+      userID3: _userID3
+    })
+    .then(() => {
+      //console.log(_highScore1 + " " + _highScore2 + " " + _highScore3);
+      console.log('sethighscore success');
+    })
+    .catch((error) => {
+      console.log('sethighscore failed');
+    });
+  });
 }
---
+++ public/account/write.js
@@ -0,0 +1,57 @@
+import { ref, onValue, getDatabase} from "https://www.gstatic.com/firebasejs/9.8.2/firebase-database.js"
+import { app } from "./app.js"
+import { getParam } from "./highScore.js"
+
+/* 7.03 kakishima
+
+------------------------
+arg : none
+ret : none
+------------------------
+プライベート変数
+_database: 呼び出したデータベース
+_ref: databaseの場所
+_ref2: databaseの場所
+_url: 現在のページのURL
+_highScore: ログインしているユーザのハイスコア
+_highScore1: 現時点で1位のハイスコア
+_highScore2: 現時点で2位のハイスコア
+_highScore3: 現時点で3位のハイスコア
+_userID: ログインしているのユーザID
+_userID1: 現時点で1位のユーザID
+_userID2: 現時点で2位のユーザID
+_userID3: 現時点で3位のユーザID
+_html: 全体のハイアスコアを表示
+_html2: ログインしているユーザのユーザID・ハイスコアを表示
+------------------------
+ウィンドウが更新された時、全体でのハイスコアとログインしているユーザのユーザID・ハイスコアを表示
+*/
+
+window.onload = function writeHighScore(){
+  const _database = getDatabase(app);
+  const _ref = ref(_database, '/data/' + 'ranking/');
+  onValue(_ref, (snapshot) => {
+    let _highScore1 = parseInt(snapshot.val().highScore1);
+    let _userID1 = snapshot.val().userID1;
+    let _highScore2 = parseInt(snapshot.val().highScore2);
+    let _userID2 = snapshot.val().userID2;
+    let _highScore3 = parseInt(snapshot.val().highScore3);
+    let _userID3 = snapshot.val().userID3;
+    var _html = "";
+    _html += "--HIGHSCORE--" + "<br>";
+    _html += "1 : " + _userID1 + " " + _highScore1 + "<br>";
+    _html += "2 : " + _userID2 + " " + _highScore2 + "<br>";
+    _html += "3 : " + _userID3 + " " + _highScore3 + "<br><br>";
+    document.getElementById("write").innerHTML = _html;
+
+    const _url = window.location.href;
+    const _userID = getParam("userID", _url);
+    const _ref2 = ref(_database, '/users/' + _userID);
+    onValue(_ref2, (snapshot) => {
+      const _highScore = parseInt(snapshot.val().highScore);
+      var _html2 = "";
+      _html2 += "YourUserID : " + _userID  + "<br>" + "YourHighScore : " + _highScore + "<br>";
+      document.getElementById("write2").innerHTML = _html2;
+    })
+  })
+}
